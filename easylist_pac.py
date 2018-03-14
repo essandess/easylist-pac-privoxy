8369f44e6cf7589bd1cba3d5013a32a98c8ae6d5
Steven Thomas Smith <s.t.smith@ieee.org>
2018-03-14 19:50:59 -0400

    iOS optimizations for hash and regex lengths

--- easylist_pac.py
+++ easylist_pac.py
@@ -92,9 +92,9 @@ class EasyListPAC:
         parser.add_argument('-rg', '--good-rule-max', help="Maximum number of good rules (-1 for unlimited)",
                             type=int, default=1099)
         parser.add_argument('-th', '--truncate_hash', help="Truncate hash object length to maximum number", type=int,
-                            default=4999)
+                            default=3999)
         parser.add_argument('-tr', '--truncate_regex', help="Truncate regex rules to maximum number", type=int,
-                            default=1099)
+                            default=499)
         parser.add_argument('-w', '--sliding-window', help="Sliding window training and test (slow)", action='store_true')
         parser.add_argument('-x', '--Extra_EasyList_URLs', help="Extra Easylsit URLs", type=str, nargs='+', default=[])
         parser.add_argument('-*', '--wildcard-limit', help="Limit the number of wildcards", type=int, default=999)
